8e411fa760bb05a8fa17c3df5f55be0e32b6d6a1
Teo Perisanu <Teo.Perisanu@analog.com>
2022-01-05 17:34:10 +0200

    ci: Add Windows builds.
    Add builds for Windows (VS 16 2019) 32 & 64.
    Add installer.
    
    Signed-off-by: Teo Perisanu <Teo.Perisanu@analog.com>

---
+++ ci/azure/install_deps_win.ps1
@@ -0,0 +1,32 @@
+$COMPILER=$Env:COMPILER
+$ARCH=$Env:ARCH
+
+$src_dir=$pwd
+
+
+if (!(Test-Path deps)) {
+	mkdir deps
+}
+cd deps
+
+echo "Build deps..."
+
+git clone --depth 1 https://github.com/analogdevicesinc/libtinyiiod.git
+cd libtinyiiod
+if ($ARCH -eq "Win32") {
+    mkdir build-win32
+    cd build-win32
+} else {
+    mkdir build-x64
+    cd build-x64
+}
+
+cmake -G "$COMPILER" -A "$ARCH" -DCMAKE_SYSTEM_PREFIX_PATH="C:" -DBUILD_EXAMPLES=OFF ..
+cmake --build . --target install --config Release
+
+echo "Downloading deps..."
+cd C:\
+wget http://swdownloads.analog.com/cse/build/libiio-win-deps-libusb1.0.24.zip -OutFile "libiio-win-deps.zip"
+7z x -y "C:\libiio-win-deps.zip"
+
+set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
